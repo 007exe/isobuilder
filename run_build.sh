b9fda1f0788354abc0bc28e3eaa3880c8bf9e72a
aix27249 <i27249@gmail.com>
2012-09-06 03:50:52 +0400

    added support for testing. Needs testing right now :)

--- run_build.sh
+++ run_build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Script for building full AgiliaLinux ISO images
+# 
+# Available environment options:
+# ARCH: target architecture (x86_64 or x86, default: host arch)
+# ISOBUILD: isobuild used to build image (default: openbox)
+# USE_TESTING: enables testing packages. Values: 1, 0; default: not set
+
+
 set -e
 # Getting current directory, date and host arch
 CWD=`pwd`
@@ -15,23 +24,43 @@ else
 fi
 
 TREE=$CWD/RSYNC/$arch
+TESTING_TREE=$CWD/RSYNC/${arch}-testing
 LIVECONT=$CWD/BUILD
 NAME=agilia_live
 LIVEROOT=$LIVECONT/$NAME-$arch
 ISODIR=$CWD/iso
 ISONAME=AgiliaLinux-$arch-$date.iso
+REPO=file:///$TREE/repository/
 
+if [ "$USE_TESTING" = "1" ] ; then
+	REPO="$REPO file:///$TESTING_TREE/repository"
+fi
 
 # First of all, rsync please
 # Check if directory exists, if not - create it and download rsync script
 if [ ! -f $TREE/rsync-update.sh ] ; then
 	echo "No RSYNC script, creating it"
 	mkdir -p $TREE
-	( cd $TREE ; wget http://packages.agilialinux.ru/core/8.0/$arch/rsync-update.sh )
+	( cd $TREE && wget http://packages.agilialinux.ru/core/8.0/$arch/rsync-update.sh )
 fi
+
+if [ "$USE_TESTING" = "1" ] ; then
+	echo "RSyncing testing repo, wait please"
+	if [ ! -f $TESTING_TREE/rsync-update.sh ] ; then
+		mkdir -p $TESTING_TREE
+		( cd $TESTING_TREE && wget http://packages.agilialinux.ru/testing/8.0/$arch/rsync-update.sh )
+	fi
+fi
+
+
 
 # Do the sync
 ( cd $TREE && sh ./rsync-update.sh )
+
+if [ "$USE_TESTING" = "1" ] ; then
+	( cd $TESTING_TREE && sh ./rsync-update.sh)
+fi
+
 
 # Clean previous builds
 echo "Cleaning previous build directory: $LIVEROOT"
@@ -41,7 +70,7 @@ rm -rf $LIVEROOT
 # Now, build rootfs
 echo "Building live system"
 ( cd ISOBUILDS/${ISOBUILD}
-	iso_name=$NAME arch=$arch REPO=file:///$TREE/repository/ mklivecd -l $LIVECONT -a
+	iso_name=$NAME arch=$arch REPO="$REPO" mklivecd -l $LIVECONT -a
 )
 
 # Copy new files to original tree
@@ -49,7 +78,11 @@ cp -v $LIVEROOT/boot/initrd${bits}.img $TREE/boot/
 cp -v $LIVEROOT/boot/vmlinuz${bits} $TREE/boot/
 cp -v $LIVEROOT/fs${bits}/rootfs.sfs $TREE/fs${bits}/
 
-
+# If testing is used, copy testing package tree and re-generate index
+if [ "$USE_TESTING" = "1" ] ; then
+	mkdir -p $TREE/repository/_testing
+	rsync -arvh --progress $TESTING_TREE/repository $TREE/repository/_testing
+	( cd $TREE ; mpkg-index )
 
 # Create final ISO
 mkdir -p $ISODIR
